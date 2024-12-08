3b863205f1a56210e3c01db3978d80d6c3622916
Hung Nguyen <nthung2112@gmail.com>
2024-12-08 14:42:56 +0700

    Add support vi

--- app/src/main/java/com/fongmi/android/tv/utils/LanguageUtil.java
+++ app/src/main/java/com/fongmi/android/tv/utils/LanguageUtil.java
@@ -18,6 +18,7 @@ public class LanguageUtil {
     }
 
     public static int locale() {
+        if (Locale.getDefault().getLanguage().equals("vi")) return 3;
         if (!Locale.getDefault().getLanguage().equals("zh")) return 0;
         if (Locale.getDefault().getCountry().equals("CN")) return 1;
         return 2;
@@ -26,6 +27,7 @@ public class LanguageUtil {
     public static Locale getLocale(int lang) {
         if (lang == 1) return Locale.SIMPLIFIED_CHINESE;
         else if (lang == 2) return Locale.TRADITIONAL_CHINESE;
+        else if (lang == 3) return new Locale("vi"); // Vietnamese locale
         else return Locale.ENGLISH;
     }
 
